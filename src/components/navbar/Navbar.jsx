027d00224aa55eb03733825a89c41789e700ff06
Khang <khangb1910234@student.ctu.edu.vn>
2022-11-10 10:59:03 +0700

    count hotels

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Kiawbooking</span>
+        <span  className="logo">Kiawbooking</span>
         <div className="navItems">
             <button className="navButton">Đăng Ký</button>
             <button className="navButton"> Đăng Nhập</button>
--- src/components/special/Special.jsx
+++ src/components/special/Special.jsx
@@ -1,39 +1,47 @@
+import useFetch from "../../hooks/useFetch"
 import "./special.css"
 
 const Special = () => {
+
+  const {data, loading, error} = useFetch("/hotels/countByCity?cities=berlin,madrid,london")
+
   return (
     <div className="special">
-      <div className="specialItem">
-        <img src="https://cf.bstatic.com/xdata/images/city/600x500/688849.jpg?k=c1f2770a0c096aeb09a6956a9754abbb61b946a932601a4a02bb6aa42d7fa74a&o=" alt="" className="specialImg" />
-        <div className="specialTitles">
-            <h1>Hà Nội</h1>
-            <h2>50 địa điểm</h2>
+     {loading ? (
+      "Loading is wait"
+      ) : 
+      <> 
+        <div className="specialItem">
+          <img src="https://cf.bstatic.com/xdata/images/city/600x500/688849.jpg?k=c1f2770a0c096aeb09a6956a9754abbb61b946a932601a4a02bb6aa42d7fa74a&o=" alt="" className="specialImg" />
+          <div className="specialTitles">
+              <h1>Berlin</h1>
+              <h2>{data[0]} địa điểm</h2>
+          </div>
         </div>
-      </div>
 
-      <div className="specialItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/600x500/688892.webp?k=ab98c6d1cae80333bbd3129cfeb692f9fd1b17caa359d2fb4fdf35a4160e5ccf&o="
-          alt=""
-          className="specialImg"
-        />
-        <div className="specialTitles">
-          <h1>TP. Hồ Chí Minh</h1>
-          <h2>231 địa điểm</h2>
+        <div className="specialItem">
+          <img
+            src="https://cf.bstatic.com/xdata/images/city/600x500/688892.webp?k=ab98c6d1cae80333bbd3129cfeb692f9fd1b17caa359d2fb4fdf35a4160e5ccf&o="
+            alt=""
+            className="specialImg"
+          />
+          <div className="specialTitles">
+            <h1>Madrid</h1>
+            <h2>{data[1]} địa điểm</h2>
+          </div>
         </div>
-      </div>
 
-      <div className="specialItem">
-        <img
-          src="https://cf.bstatic.com/xdata/images/city/600x500/688906.webp?k=6b660c5d295f53392879d35893e48b7cf96be8311cca62eb903eaaed02090ad2&o="
-          alt=""
-          className="specialImg"
-        />
-        <div className="specialTitles">
-          <h1>Nha Trang</h1>
-          <h2>78 địa điểm</h2>
-        </div>
-      </div>
+        <div className="specialItem">
+          <img
+            src="https://cf.bstatic.com/xdata/images/city/600x500/688906.webp?k=6b660c5d295f53392879d35893e48b7cf96be8311cca62eb903eaaed02090ad2&o="
+            alt=""
+            className="specialImg"
+          />
+          <div className="specialTitles">
+            <h1>London</h1>
+            <h2>{data[2]} địa điểm</h2>
+          </div>
+      </div> </>}
     </div>
     
   )
